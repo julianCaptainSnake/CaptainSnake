8eaac359ec70c16112c307b9b0e28ad0512a5786
julianCaptainSnake <96374@fhwn.ac.at>
2016-06-13 15:23:36 +0200

    Player Control Keyboard - Class + Event

---
+++ CaptainSnakeServer/ControlPlayerOnKeyPressedEventArgs.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ControlPlayerOnKeyPressedEventArgs.cs" company="FH Wiener Neustadt">
+//     Copyright (c) FH Wiener Neustadt. All rights reserved.
+// </copyright>
+// <summary>Represents the ControlPlayerOnKeyPressedEventArgs class.</summary>
+//-----------------------------------------------------------------------
+
+namespace CaptainSnakeServer
+{
+    /// <summary>
+    /// Represents the <see cref="ControlPlayerOnKeyPressedEventArgs"/> class.
+    /// </summary>
+    public class ControlPlayerOnKeyPressedEventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ControlPlayerOnKeyPressedEventArgs"/> class.
+        /// </summary>
+        public ControlPlayerOnKeyPressedEventArgs(InputTypes itype)
+        {
+            this.ControlInput = itype;
+        }
+
+        /// <summary>
+        /// Gets or sets the input type of the user input.
+        /// </summary>
+        /// <value>The input type of the user input</value>
+        public InputTypes ControlInput
+        {
+            get;
+            set;
+        }
+    }
+}
