902c8a94d909682d20597117d71b28d458a35cda
CaptainLukas <98411@fhwn.ac.at>
2016-06-13 14:55:56 +0200

    Input types co

--- CaptainSnakeServer/InputTypes.cs
+++ CaptainSnakeServer/InputTypes.cs
@@ -1,15 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="InputTypes.cs" company="CaptainSnake">
+//     Team 1.
+// </copyright>
+// <summary>This is the InputTypes class.</summary>
+//-----------------------------------------------------------------------
 namespace CaptainSnakeServer
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// The InputTypes enum.
+    /// </summary>
     public enum InputTypes
     {
+        /// <summary>
+        /// The up direction.
+        /// </summary>
         Up,
+        
+        /// <summary>
+        /// The down direction.
+        /// </summary>
         Down,
+        
+        /// <summary>
+        /// A shot.
+        /// </summary>
         Shoot,
+
+        /// <summary>
+        /// The layer change.
+        /// </summary>
         ChangeLayer
     }
 }
