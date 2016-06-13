7e7053b6ece8663fdd0656ad0d02e89dabd7b0ef
CaptainLukas <98411@fhwn.ac.at>
2016-06-13 14:48:42 +0200

    alles

--- CaptainSnakeServer/EnhancedTcpListenerOnClientConnectedEventArgs.cs
+++ CaptainSnakeServer/EnhancedTcpListenerOnClientConnectedEventArgs.cs
@@ -1,21 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="EnhancedTcpListenerOnClientConnectedEventArgs.cs" company="CaptainSnake">
+//     Team 1.
+// </copyright>
+// <summary>This is the EnhancedTcpListenerOnClientConnectedEventArgs class.</summary>
+//-----------------------------------------------------------------------
 namespace CaptainSnakeServer
 {
-    public class EnhancedTcpListenerOnClientConnectedEventArgs
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Sockets;
+    using System.Text;
+
+    /// <summary>
+    /// The EnhancedTcpListenerOnClientConnectedEventArgs class.
+    /// </summary>
+    public class EnhancedTcpListenerOnClientConnectedEventArgs : EventArgs
     {
-        public NetworkStream stream
+        /// <summary>
+        /// The network stream.
+        /// </summary>
+        private NetworkStream stream;
+
+        /// <summary>
+        /// Gets or sets a network stream.
+        /// </summary>
+        public NetworkStream Stream
         {
             get
             {
-                throw new System.NotImplementedException();
+                return this.stream;
             }
 
             set
             {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(value), "The stream is null.");
+                }
+
+                this.stream = value;
             }
         }
     }
