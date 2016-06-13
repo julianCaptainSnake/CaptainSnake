d12562633518ccf93a3a9170bee8d4314ee0ecb6
JCaptainSnake <98956@fhwn.ac.at>
2016-06-13 15:23:42 +0200

    Stylecop Direction

--- CaptainSnakeClient/Direction.cs
+++ CaptainSnakeClient/Direction.cs
@@ -1,19 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Direction.cs" company="CaptainSnake">
+//     Team 1.
+// </copyright>
+// <summary>This is the direction class.</summary>
+//-----------------------------------------------------------------------
 
 namespace CaptainSnakeClient
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+
     public enum Direction
     {
+        /// <summary>
+        /// The up direction.
+        /// </summary>
         Up,
+
+        /// <summary>
+        /// The down direction.
+        /// </summary>
         Down,
+
+        /// <summary>
+        /// The left direction.
+        /// </summary>
         Left,
+
+        /// <summary>
+        /// The right direction.
+        /// </summary>
         Right,
+
+        /// <summary>
+        /// The up right directin.
+        /// </summary>
         UpRight,
+
+        /// <summary>
+        /// The up left direction
+        /// </summary>
         UpLeft,
+
+        /// <summary>
+        /// The down right direction.
+        /// </summary>
         DownRight,
+
+        /// <summary>
+        /// The down left direction.
+        /// </summary>
         DownLeft
     }
 }
